55202b9c8918d5ed785bfb8526e5930c7c46eaab
DorkmanX <kamil.rzezniczek@gmail.com>
2022-02-03 20:19:44 +0100

    First commit

--- server.py
+++ server.py
@@ -14,7 +14,7 @@ def count(number):
 def hello():
     a=str(count(5))
     message={"answer":a}
-    return jsonify(a)
+    return str(a)
 
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run()
